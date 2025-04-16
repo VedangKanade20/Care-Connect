a96b74f9ea3270b64bae9ab57910c5d6e3b056fd
vedang Kanade <vedangk20@gmail.com>
2025-04-17 01:14:22 +0530

    Started with the models

---
+++ backend/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["Donor", "NGO_Staff", "Admin"],
+    required: true,
+  },
+  staffType: {
+    type: String,
+    enum: ["core", "delivery"],
+    required: function () {
+      return this.role == "NGO_Staff";
+    },
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
