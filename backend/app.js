9a00ee94dbd86122c704d739844d57a39c66cde7
vedang Kanade <vedangk20@gmail.com>
2025-04-20 13:11:31 +0530

    A minor change

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.send("hello , world");
 });
 
